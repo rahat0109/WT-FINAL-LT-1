8b2a438742050c34c224000a02b46cd0457890a0
Hasibul Islam <114330667+rahat0109@users.noreply.github.com>
2022-11-13 13:46:53 +0600

    Add files via upload

---
+++ history.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+</head>
+<body>
+        <center>
+        <h1>Page3[History]</h1><br></br>
+</center>
+
+<h3>History</h3><br>
+1.<a href="home.php"> <b>  Home </b></a> 
+2.<a href="Conversion.php"> <b>  Conversion Rate </b></a> 
+3.<a href="history.php"><b> History </b></a><br><br>
+
+
+
+</html>
---
+++ home.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+</head>
+<body>
+        <center>
+        <h1>Page1[Home]</h1><br></br>
+</center>
+<h3>Converter</h3><br>
+1.<a href="home.php"> <b>  Home </b></a> 
+2.<a href="Conversion.php"> <b>  Conversion Rate </b></a> 
+3.<a href="history.php"><b> History </b></a><br><br>
+
+ <label for="uname"><b>value</b></label>
+      <input type="text" placeholder="" name="uname" required>
+	  <br><br>
+
+<h3>Converter :</h3><br>
+Select Catagory: <select name="Catagory" id="Catagory">
+			
+			<option value="Usd to Bdt">Usd to Bdt</option>
+			<option value="Bdt to Usd">Bdt to Usd</option>
+			<button type="submit">submit</button>    <br><br>
+			
+			<label for="quantity">Value :</label>
+<input type="number" id="quantity" name="quantity" >
+		<input type="submit" name="submit" value="Submit"><br></br>
+                <label for="phone">Value:</label>
+<input type="tel" id="phone" name="phone" >
+	  
+			
+</html>
+
+
